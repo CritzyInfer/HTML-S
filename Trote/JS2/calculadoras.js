c528c60932a02e6c2f3961c4115380b50e3cbfc4
CritzyInfer <pedro.s.rodrigues6@gmial.com>
2025-03-24 20:41:38 -0300

    24/03

---
+++ Trote/JS2/calculadoras.js
@@ -0,0 +1,7 @@
+    function calcular(){
+        //delcarar variavel sem tipo
+        let acaoSocial = document.getElementById("acaoSocial").value
+        let Homenagem = document.getElementById("Homenagem").value
+        let soma = Number(acaoSocial) + Number(Homenagem)
+        alert(soma)
+    }
