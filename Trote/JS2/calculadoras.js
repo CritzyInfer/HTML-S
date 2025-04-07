9dc5770777e472ed1fa103d418220b91b73fdc27
CritzyInfer <pedroh.srodrigues6@gmail.com>
2025-04-06 23:27:34 -0300

    06/04

--- Trote/JS2/calculadoras.js
+++ Trote/JS2/calculadoras.js
@@ -1,107 +1,95 @@
 function calcular() {
-    // Declarar variáveis
-    let soma = 0;
-    let acaoSocial = document.getElementById("acaoSocial").value;
-    soma = soma + Number(acaoSocial);
 
-    let Homenagem = document.getElementById("Homenagem").value;
-    soma = soma + Number(Homenagem);
+    let mascote, homenagem, leite, kit, suplemento, soma, equipe
 
-    let Leite = document.getElementById("Leite").value;
-    soma = soma + (2 * Number(Leite));
+    mascote = Number(document.getElementById("Mascote").value)
 
-    let Kit = document.getElementById("kit").value;
-    let suple = document.getElementById("Suple").value;
+    homenagem = Number(document.getElementById("homenagem").value)
 
-    let equipes = document.getElementById("equipe").value;
+    leite = Number(document.getElementById("leite").value)
 
-    
-    let pontosKit = 0; 
-    if (equipes === "Laranja") {
+    kit = Number(document.getElementById("kit").value)
 
-        Kit = Number(Kit); 
-        suple = Number(suple);
-        
-        if (Kit >= 97 && suple >= 49) {
-            pontosKit = 5000 + ((Kit - 97) * 30) + ((suple - 49) * 15);
+    suplemento = Number(document.getElementById("suplemento").value)
 
-        } else if (Kit >= 78 && suple >= 39) {
-            pontosKit = 4000 + ((Kit - 78) * 30) + ((suple - 39) * 15);
+    soma = mascote + homenagem + (2 * (leite))
 
-        } else if (Kit >= 19 && suple >= 10) {
-            pontosKit = 1000 + ((Kit - 19) * 30) + ((suple - 10) * 15);
-}
+    equipe = document.getElementById("equipe").value
+    if (equipe == "Laranja") {
+        if (kit >= 97 && suplemento >= 49) {
+            soma = soma + 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
+        }
+        else if (kit >= 78 && suplemento >= 39) {
+            soma = soma + 4000 + ((kit - 78) * 30) + ((suplemento - 39) * 15)
+        }
+        else if (kit >= 49 && suplemento >= 25) {
+            soma = soma + 2500 + ((kit - 49) * 30) + ((suplemento - 25) * 15)
+        }
+        else if (kit >= 19 && suplemento >= 10) {
+            soma = soma + 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
+        }
     }
-    if (equipes === "Preto") {
-        Kit = Number(Kit); // Certificar que Kit é um número
-        suple = Number(suple); // Certificar que Suple é um número
-        if (Kit >= 103 && suple >= 52) {
-            pontosKit = 5000 + ((Kit - 103) * 30) + ((suple - 52) * 15);
-        } else if (Kit >= 82 && suple >= 39) {
-            pontosKit = 4000 + ((Kit - 82) * 30) + ((suple - 39) * 15);
-        } else if (Kit >= 21 && suple >= 10) {
-            pontosKit = 1000 + ((Kit - 21) * 30) + ((suple - 10) * 15);
-}
+
+    if (equipe == "Preto") {
+        if (kit >= 103 && suplemento >= 52) {
+            soma = soma + 5000 + ((kit - 103) * 30) + ((suplemento - 52) * 15)
+        }
+        else if (kit >= 82 && suplemento >= 42) {
+            soma = soma + 4000 + ((kit - 82) * 30) + ((suplemento - 42) * 15)
+        }
+        else if (kit >= 52 && suplemento >= 26) {
+            soma = soma + 2500 + ((kit - 52) * 30) + ((suplemento - 26) * 15)
+        }
+        else if (kit >= 21 && suplemento >= 10) {
+            soma = soma + 1000 + ((kit - 21) * 30) + ((suplemento - 10) * 15)
+        }
     }
-    if (equipes === "Roxa") {
-        Kit = Number(Kit); // Certificar que Kit é um número
-        suple = Number(suple); // Certificar que Suple é um número
-        if (Kit >= 102 && suple >= 51) {
-            pontosKit = 5000 + ((Kit - 102) * 30) + ((suple - 51) * 15);
 
-        } else if (Kit >= 82 && suple >= 41) {
-            pontosKit = 4000 + ((Kit - 82) * 30) + ((suple - 41) * 15);
-
-        } else if (Kit >= 20 && suple >= 10) {
-            pontosKit = 1000 + ((Kit - 20) * 30) + ((suple - 10) * 15);
-}
+    if (equipe == "Roxo") {
+        if (kit >= 102 && suplemento >= 51) {
+            soma = soma + 5000 + ((kit - 102) * 30) + ((suplemento - 51) * 15)
+        }
+        else if (kit >= 82 && suplemento >= 41) {
+            soma = soma + 4000 + ((kit - 82) * 30) + ((suplemento - 41) * 15)
+        }
+        else if (kit >= 51 && suplemento >= 26) {
+            soma = soma + 2500 + ((kit - 51) * 30) + ((suplemento - 26) * 15)
+        }
+        else if (kit >= 20 && suplemento >= 10) {
+            soma = soma + 1000 + ((kit - 20) * 30) + ((suplemento - 10) * 15)
+        }
     }
-    if (equipes === "Verde") {
-        Kit = Number(Kit); // Certificar que Kit é um número
-        suple = Number(suple); // Certificar que Suple é um número
-        if (Kit >= 88 && suple >= 44) {
-            pontosKit = 5000 + ((Kit - 88) * 30) + ((suple - 44) * 15);
-        } else if (Kit >= 70 && suple >= 35) {
-            pontosKit = 4000 + ((Kit - 70) * 30) + ((suple - 35) * 15);
-        } else if (Kit >= 44 && suple >= 22) {
-            pontosKit = 1000 + ((Kit - 19) * 30) + ((suple - 10) * 15);
-}
+
+    if (equipe == "Verde") {
+        if (kit >= 103 && suplemento >= 52) {
+            soma = soma + 5000 + ((kit - 103) * 30) + ((suplemento - 52) * 15)
+        }
+        else if (kit >= 82 && suplemento >= 42) {
+            soma = soma + 4000 + ((kit - 82) * 30) + ((suplemento - 42) * 15)
+        }
+        else if (kit >= 52 && suplemento >= 26) {
+            soma = soma + 2500 + ((kit - 52) * 30) + ((suplemento - 26) * 15)
+        }
+        else if (kit >= 21 && suplemento >= 10) {
+            soma = soma + 1000 + ((kit - 21) * 30) + ((suplemento - 10) * 15)
+        }
     }
-    if (equipes === "Vermelho") {
-        Kit = Number(Kit); // Certificar que Kit é um número
-        suple = Number(suple); // Certificar que Suple é um número
-        if (Kit >= 93 && suple >= 47) {
-            pontosKit = 5000 + ((Kit - 97) * 30) + ((suple - 49) * 15);
-        } else if (Kit >= 74 && suple >= 38) {
-            pontosKit = 4000 + ((Kit - 74) * 30) + ((suple - 38) * 15);
-        } else if (Kit >= 19 && suple >= 9) {
-            pontosKit = 1000 + ((Kit - 19) * 30) + ((suple - 9) * 15);
-}
+
+    if (equipe == "Vermelho") {
+        if (kit >= 93 && suplemento >= 47) {
+            soma = soma + 5000 + ((kit - 93) * 30) + ((suplemento - 47) * 15)
+        }
+        else if (kit >= 74 && suplemento >= 38) {
+            soma = soma + 4000 + ((kit - 74) * 30) + ((suplemento - 38) * 15)
+        }
+        else if (kit >= 47 && suplemento >= 24) {
+            soma = soma + 2500 + ((kit - 47) * 30) + ((suplemento - 24) * 15)
+        }
+        else if (kit >= 19 && suplemento >= 9) {
+            soma = soma + 1000 + ((kit - 19) * 30) + ((suplemento - 9) * 15)
+        }
     }
-     // Adicionar pontosKit ao total
-     soma = soma + pontosKit;
-
-    // Continuar somando os outros itens
-    let Arroz5 = document.getElementById("PacArroz5kg").value;
-    soma = soma + (5 * Number(Arroz5));
-
-    let Arroz1 = document.getElementById("PacArroz1kg").value;
-    soma = soma + Number(Arroz1);
-
-    let Feijao2 = document.getElementById("PacFej2kg").value;
-    soma = soma + (2 * Number(Feijao2));
-
-    let Feijao1 = document.getElementById("PacFej1kg").value;
-    soma = soma + Number(Feijao1);
-
-    let Macarrao = document.getElementById("Macarrao").value;
-    soma = soma + (0.5 * Number(Macarrao));
-
-    let Oleo = document.getElementById("Oleo").value;
-    soma = soma + Number(Oleo);
-
-   
-
-    // Exibir o valor da soma
-    document.getElementById("soma").innerHTML = soma.toFixed(2);
+    // retorna o valor ao HTML
+    // template string
+    document.getElementById("soma").innerHTML = `A soma é ${soma}`
 }
